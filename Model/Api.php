cf026318bbcc19f8f63c4a1ff978f79c0aaa7c88
Evan Jacobs <evan.jacobs2@gmail.com>
2016-03-14 12:01:34 -0400

    Api interface improvements

--- Model/Api.php
+++ Model/Api.php
@@ -15,49 +15,24 @@ use Springbot\Main\Helper\Data;
  */
 class Api extends AbstractModel
 {
+    const ETL_URL = 'http://localhost:8080/';
+    const STORE_REGISTRATION_URL = 'webhooks/v1/stores';
+
     const SUCCESSFUL_RESPONSE = 'ok';
     const HTTP_CONTENT_TYPE = 'Content-type: application/json';
     const TOTAL_POST_FAIL_LIMIT = 32;
     const RETRY_LIMIT = 3;
 
-    /**
-     * @var string
-     */
-    private $_securityToken;
-
-    /**
-     * @var int
-     */
     private $_retries;
-
-    /**
-     * @var Data
-     */
     private $_springbotHelper;
-
-    /**
-     * @var ScopeConfigInterface
-     */
     private $_scopeConfig;
-
-    /**
-     * @var \Zend_Http_client
-     */
     private $_client;
-
-    /**
-     * @var string
-     */
-    private $_url;
-
-    /**
-     * @var EncryptorInterface
-     */
     private $_encrypt;
 
     /**
      * Api constructor.
      * @param Data $springbotHelper
+     * @param EncryptorInterface $encryptorInterface
      * @param ScopeConfigInterface $scopeConfig
      * @param Context $context
      * @param Registry $registry
@@ -76,16 +51,11 @@ class Api extends AbstractModel
     }
 
     /**
-     * @param $model
-     * @param $data
-     * @return string
+     * @return EncryptorInterface
      */
-    public function wrap($model, $data)
+    public function encryptor()
     {
-        $transport = new \stdClass();
-        $transport->$model = $data;
-
-        return \Zend_Json::encode($transport, false, ['enableJsonExprFinder' => true]);
+        return $this->_encrypt;
     }
 
     /**
@@ -94,51 +64,47 @@ class Api extends AbstractModel
     public function reinit()
     {
         $this->_retries = 0;
-
         return $this;
     }
 
     /**
-     * @param $method
-     * @param bool $payload
-     * @param bool $authenticate
-     * @param string $httpMethod
-     * @return array|mixed
+     * @param $url
+     * @param $body
+     * @param array $headers
+     * @return \Zend_Http_Response
      * @throws \Exception
      * @throws \Zend_Http_Client_Exception
      */
-    public function call($method, $payload = false, $authenticate = true, $httpMethod = \Zend_Http_Client::POST)
+    public function post($url, $body, $headers = ['Content-Type' => 'application/json'])
     {
-        $result = [];
-        $client = $this->getClient($httpMethod);
-        $client->setUri($this->getApiUrl($method));
-
-        if ($authenticate) {
-            $this->authenticate();
-            $client->setHeaders('X-AUTH-TOKEN:' . $this->_springbotHelper->getSecurityToken());
-        }
-
-        if ($payload) {
-            $client->setRawData(utf8_encode($payload));
-        }
-
         try {
-            $response = $client->request();
-
-            if ($response->isSuccessful()) {
-                $result = json_decode($response->getBody(), true);
-
-                if ($result['status'] != self::SUCCESSFUL_RESPONSE) {
-                    $this->_logger->error($response->getBody());
-                }
-            }
+            return $this->getClient(\Zend_Http_Client::POST)
+                ->setUri($url)
+                ->setHeaders($headers)
+                ->setRawData($body)
+                ->request();
         } catch (\Exception $e) {
-            $code = isset($result['status']) ? $result['status'] : 'null';
-
-            throw new \Exception("$method call failed with code: $code");
+            throw new \Exception("HTTP POST failed with code: {$e->getMessage()}");
         }
+    }
 
-        return $result;
+    /**
+     * @param $url
+     * @param array $headers
+     * @return \Zend_Http_Response
+     * @throws \Exception
+     * @throws \Zend_Http_Client_Exception
+     */
+    public function get($url, $headers = [])
+    {
+        $client = $this->getClient(\Zend_Http_Client::POST)
+            ->setUri($url)
+            ->setHeaders($headers);
+        try {
+            return $client->request();
+        } catch (\Exception $e) {
+            throw new \Exception("HTTP GET failed with code: {$e->getMessage()}");
+        }
     }
 
     /**
@@ -151,83 +117,17 @@ class Api extends AbstractModel
         $this->_client = new \Zend_Http_Client();
         $this->_client->setMethod($method);
         $this->_client->setHeaders(self::HTTP_CONTENT_TYPE);
-
         return $this->_client;
     }
 
     /**
-     * @param string $method
+     * @param string $subpath
      * @return string
      */
-    public function getApiUrl($method = '')
+    public function getApiUrl($subpath = '')
     {
-        if (!isset($this->_url)) {
-            $this->_url = $this->_scopeConfig->getValue('springbot/configuration/api_url');
-            if (!$this->_url) {
-                $this->_url = 'https://api.springbot.com/';
-            }
-            $this->_url .= 'api/';
-        }
-        return $this->_url . $method;
+        return self::ETL_URL . $subpath;
     }
 
-    /**
-     * @throws \Exception
-     */
-    public function authenticate()
-    {
-        if (!$this->hasToken()) {
-            $credentials = $this->_getApiCredentials();
-            $result = $this->call('registration/login', $credentials, false);
 
-            if (!isset($result['token'])) {
-                throw new \Exception('Token not available in api response. Please check springbot credentials.');
-            }
-
-            $this->_securityToken = $result['token'];
-        }
-
-        return;
-    }
-
-    public function apiPostWrapped($model, $struct, $arrayWrap = false)
-    {
-        if ($arrayWrap) {
-            $struct = [$struct];
-        }
-
-        $payload = $this->wrap($model, $struct);
-
-        return $this->reinit()->call($model, $payload);
-    }
-
-    public function hasToken()
-    {
-        $token = $this->_scopeConfig->getValue('springbot/config/security_token');
-        if ($token) {
-            $this->_securityToken = $token;
-        }
-
-        return isset($this->_securityToken);
-    }
-
-    protected function _getApiCredentials()
-    {
-        $post = [
-            'user_id' => $this->_getAccountEmail(),
-            'password' => $this->_getAccountPassword(),
-        ];
-        return json_encode($post);
-    }
-
-    protected function _getAccountEmail()
-    {
-        return $this->_scopeConfig->getValue('springbot/config/account_email');
-    }
-
-    protected function _getAccountPassword()
-    {
-        $passwd = $this->_scopeConfig->getValue('springbot/config/account_password');
-        return $this->_encrypt->decrypt($passwd);
-    }
 }
