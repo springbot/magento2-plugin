400749ed4bb6cacd65a0036915d221558bde961e
Evan Jacobs <ejacobs@springbot.com>
2016-05-11 20:58:34 -0400

    Fixed cart reconstitution observer

--- Observer/cartReconstitutionObserver.php
+++ Observer/cartReconstitutionObserver.php
@@ -4,10 +4,12 @@ namespace Springbot\Main\Observer;
 
 use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Message\ManagerInterface;
+use Magento\Quote\Model\QuoteFactory;
 use Psr\Log\LoggerInterface;
 use Springbot\Main\Helper\Data as SpringbotHelper;
 
@@ -17,28 +19,35 @@ use Springbot\Main\Helper\Data as SpringbotHelper;
  */
 class CartReconstitutionObserver implements ObserverInterface
 {
-    private $_checkoutSession;
+    private $_session;
     private $_action;
     private $_messageManager;
     private $_springbotHelper;
     private $_loggerInterface;
+    private $_quoteFactory;
 
     /**
      * @param Action $action
-     * @param LoggerInterface $loggerInterface
      * @param ManagerInterface $messageManager
-     * @param Session $checkoutSession
+     * @param Session $session
+     * @param QuoteFactory $quoteFactory
+     * @param ScopeConfigInterface $scopeConfig
      * @param SpringbotHelper $springbotHelper
+     * @param LoggerInterface $loggerInterface
      */
     public function __construct(
         Action $action,
         ManagerInterface $messageManager,
-        Session $checkoutSession,
+        Session $session,
+        QuoteFactory $quoteFactory,
+        ScopeConfigInterface $scopeConfig,
         SpringbotHelper $springbotHelper,
         LoggerInterface $loggerInterface
     ) {
         $this->_action = $action;
-        $this->_checkoutSession = $checkoutSession;
+        $this->_session = $session;
+        $this->_quoteFactory = $quoteFactory;
+        $this->_scopeConfig = $scopeConfig;
         $this->_loggerInterface = $loggerInterface;
         $this->_messageManager = $messageManager;
         $this->_springbotHelper = $springbotHelper;
@@ -54,7 +63,7 @@ class CartReconstitutionObserver implements ObserverInterface
         try {
             if ($quoteId = $this->_action->getRequest()->getParam('quote_id')) {
                 $suppliedSecurityHash = $this->_action->getRequest()->getParam('sec_key');
-                $this->_springbotHelper->setQuote($quoteId, $suppliedSecurityHash);
+                $this->setQuote($quoteId, $suppliedSecurityHash);
             }
         } catch (LocalizedException $e) {
             $this->_messageManager->addError($e->getMessage());
@@ -62,5 +71,32 @@ class CartReconstitutionObserver implements ObserverInterface
             $this->_messageManager->addException($e, __('Cart Reconstitution error'));
         }
         return;
+    }
+
+    public function setQuote($quoteId, $suppliedSecurityHash)
+    {
+        // Instantiate Quote object and load the correct quote
+        $quote = $this->_quoteFactory->create();
+        $quote->load($quoteId);
+
+        // Check to make sure the cart is allowed to be restored
+        if ($this->_scopeConfig->getValue('springbot/cart_restore/do_restore') == 1) {
+            if ($quote) {
+
+                // Only set the quote if they don't already have one
+                if (!$this->_session->hasQuote()) {
+                    $quote->setIsActive(true)->save();
+                    $token = $this->_scopeConfig->getValue('springbot/configuration/security_token');
+                    $correctSecurityHash = sha1($quoteId . $token);
+                    if ($suppliedSecurityHash == $correctSecurityHash) {
+                        if ($this->_scopeConfig->getValue('springbot/cart_restore/retain_coupon') === 0) {
+                            $quote->setCouponCode('');
+                            $quote->save();
+                        }
+                        $this->_session->setQuoteId($quoteId);
+                    }
+                }
+            }
+        }
     }
 }
