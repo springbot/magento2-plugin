e4bb09fe36995e29950bc7b91237422601db3f08
Evan Jacobs <ejacobs@springbot.com>
2016-05-12 15:52:57 -0400

    Added store registration console commands

---
+++ Console/Command/ListStoresCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Springbot\Main\Console\Command;
+
+use Magento\Framework\App\State;
+use Magento\Store\Model\StoreManager;
+use Springbot\Main\Model\StoreConfiguration;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Springbot\Main\Model\Oauth;
+use Zend\Text\Table\Table as TextTable;
+
+/**
+ * Class ListStoresCommand
+ *
+ * @package Springbot\Main\Console\Command
+ */
+class ListStoresCommand extends Command
+{
+
+    private $_oauth;
+    private $_state;
+    private $_storeManager;
+    private $_storeConfig;
+
+    /**
+     * @param Oauth $oauth
+     * @param State $state
+     * @param StoreManager $storeManager
+     * @param StoreConfiguration $storeConfig
+     */
+    public function __construct(
+        Oauth $oauth,
+        State $state,
+        StoreManager $storeManager,
+        StoreConfiguration $storeConfig
+    ) {
+        $this->_state = $state;
+        $this->_oauth = $oauth;
+        $this->_storeManager = $storeManager;
+        $this->_storeConfig = $storeConfig;
+        parent::__construct();
+    }
+
+    /**
+     * Sets config for cli command
+     */
+    protected function configure()
+    {
+        $this->setName('springbot:stores:list')
+            ->setDescription('List all currently registered stores');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return string
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->_state->setAreaCode('adminhtml');
+        $table = new TextTable([
+            'columnWidths' => [25, 10, 20, 36],
+            'decorator' => 'ascii',
+            'AutoSeparate' => TextTable::AUTO_SEPARATE_HEADER,
+            'padding' => 1
+        ]);
+
+        $table->appendRow(['store_name', 'store_id', 'springbot_store_id', 'springbot_guid']);
+        foreach ($this->_storeManager->getStores() as $store) {
+            $springbotStoreId = $this->_storeConfig->getSpringbotStoreId($store->getId());
+            $springbotGuid = strtolower($this->_storeConfig->getGuid($store->getId()));
+            $table->appendRow([substr($store->getName(), 0, 23), $store->getId(), $springbotStoreId, $springbotGuid]);
+        }
+
+        $output->writeln($table->render());
+    }
+
+}
---
+++ Console/Command/RegisterStoresCommand.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Springbot\Main\Console\Command;
+
+use Magento\Framework\App\State;
+use Magento\Store\Model\StoreManager;
+use Springbot\Main\Model\StoreConfiguration;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Springbot\Main\Model\Register as Register;
+use Symfony\Component\Console\Input\InputArgument;
+use Zend\Text\Table\Table as TextTable;
+
+/**
+ * Class RegisterStoresCommand
+ *
+ * @package Springbot\Main\Console\Command
+ */
+class RegisterStoresCommand extends Command
+{
+
+    const EMAIL_ARGUMENT = '<springbot_email>';
+    const PASSWORD_ARGUMENT = '<springbot_password>';
+
+    private $_storeManager;
+    private $_storeConfig;
+
+    /**
+     * @param State $state
+     * @param StoreManager $storeManager
+     * @param Register $register
+     * @param StoreConfiguration $storeConfig
+     */
+    public function __construct(
+        State $state,
+        StoreManager $storeManager,
+        Register $register,
+        StoreConfiguration $storeConfig
+    ) {
+        $state->setAreaCode('adminhtml');
+        $this->_register = $register;
+        $this->_storeManager = $storeManager;
+        $this->_storeConfig = $storeConfig;
+        parent::__construct();
+    }
+
+    /**
+     * Sets config for cli command
+     */
+    protected function configure()
+    {
+        $this->setName('springbot:stores:register')
+            ->setDescription('Register all stores with Springbot')
+            ->addArgument(self::EMAIL_ARGUMENT, InputArgument::REQUIRED)
+            ->addArgument(self::PASSWORD_ARGUMENT, InputArgument::REQUIRED);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return string
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $table = new TextTable([
+            'columnWidths' => [25, 10, 20, 42],
+            'decorator' => 'ascii',
+            'AutoSeparate' => TextTable::AUTO_SEPARATE_HEADER,
+            'padding' => 1
+        ]);
+
+        $table->appendRow(['store_name', 'store_id', 'springbot_store_id', 'action']);
+        foreach ($this->_storeManager->getStores() as $store) {
+            $springbotStoreId = $this->_storeConfig->getSpringbotStoreId($store->getId());
+            $springbotGuid = strtolower($this->_storeConfig->getGuid($store->getId()));
+            if ($springbotStoreId && $springbotGuid) {
+                $table->appendRow([substr($store->getName(), 0, 23), $store->getId(), $springbotStoreId, 'Already registered, no action taken']);
+            }
+            else {
+                // $this->_register->registerStores($email, $password)
+            }
+        }
+
+        $output->writeln($table->render());
+
+    }
+
+}
