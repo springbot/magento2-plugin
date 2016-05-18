d1d42f8b14a68c46c13515c33a0875d0937b278c
Evan Jacobs <ejacobs@springbot.com>
2016-05-18 17:39:30 -0400

    Added additional events for coupons, rules

---
+++ Observer/CartSaveAfterObserver.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Springbot\Main\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+use Springbot\Queue\Model\Queue;
+use Magento\Framework\Event\Observer;
+use Magento\Quote\Model\Quote as MagentoQuote;
+use Springbot\Main\Model\Handler\CartHandler;
+
+class CartSaveAfterObserver implements ObserverInterface
+{
+    private $_logger;
+    private $_queue;
+
+    /**
+     * ProductSaveAfterObserver constructor
+     *
+     * @param LoggerInterface $loggerInterface
+     * @param Queue $queue
+     */
+    public function __construct(LoggerInterface $loggerInterface, Queue $queue)
+    {
+        $this->_logger = $loggerInterface;
+        $this->_queue = $queue;
+    }
+
+    /**
+     * Pull the cart data from the event
+     *
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        try {
+            $cart = $observer->getEvent()->getCart();
+            /* @var MagentoQuote $cart */
+            $cartId = $cart->getEntityId();
+            $this->_queue->scheduleJob(CartHandler::class, 'handle', [$cart->getStoreId(), $cartId]);
+            $this->_logger->debug("Created/Updated Cart ID: {$cartId}");
+        } catch (\Exception $e) {
+            $this->_logger->debug($e->getMessage());
+        }
+    }
+}
---
+++ Observer/RuleSaveAfterObserver.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Springbot\Main\Observer;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+use Springbot\Queue\Model\Queue;
+use Magento\Framework\Event\Observer;
+use Springbot\Main\Model\Handler\RuleHandler;
+use Magento\SalesRule\Model\Rule as MagentoRule;
+use Magento\Store\Model\Website as MagentoWebsite;
+use Exception;
+
+/**
+ * Class RuleSaveAfterObserver
+ * @package Springbot\Main\Observer
+ */
+class RuleSaveAfterObserver implements ObserverInterface
+{
+    private $_logger;
+    private $_queue;
+
+    /**
+     * RuleSaveAfterObserver constructor
+     *
+     * @param LoggerInterface $loggerInterface
+     * @param Queue $queue
+     */
+    public function __construct(LoggerInterface $loggerInterface, Queue $queue)
+    {
+        $this->_logger = $loggerInterface;
+        $this->_queue = $queue;
+    }
+
+    /**
+     * Pull the rule data from the event
+     *
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        try {
+            $rule = $observer->getEvent()->getRule();
+            /* @var MagentoRule $rule */
+            foreach ($rule->getWebsiteIds() as $websiteId) {
+                $website = ObjectManager::getInstance()->get('Magento\Store\Model\Website')->load($websiteId);
+                /* @var MagentoWebsite $website */
+                foreach ($website->getStoreIds() as $storeId) {
+                    $this->_queue->scheduleJob(RuleHandler::class, 'handle', [$storeId, $rule->getId()], 1);
+                    $this->_logger->debug("Scheduled sync job for Rule ID: {$rule->getId()} in store id: {$storeId}");
+                }
+            }
+
+        } catch (Exception $e) {
+            $this->_logger->debug($e->getMessage());
+        }
+    }
+}
