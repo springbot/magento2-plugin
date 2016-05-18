2984761dbd3e0118d76d0ecf4ac6c0669eecaf40
Evan Jacobs <ejacobs@springbot.com>
2016-05-18 14:00:47 -0400

    Moved observers, added order save observer

---
+++ Observer/OrderSaveObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Springbot\Main\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+use Springbot\Queue\Model\Queue;
+use Magento\Framework\Event\Observer;
+use Magento\Sales\Model\Order as MagentoOrder;
+use Springbot\Main\Model\Handler\OrderHandler;
+use Magento\Checkout\Model\Session;
+
+class OrderSaveObserver implements ObserverInterface
+{
+    private $_logger;
+    private $_queue;
+
+    /**
+     * ProductSaveAfterObserver constructor
+     *
+     * @param LoggerInterface $loggerInterface
+     * @param Queue $queue
+     */
+    public function __construct(
+        LoggerInterface $loggerInterface,
+        Queue $queue
+    ) {
+        $this->_logger = $loggerInterface;
+        $this->_queue = $queue;
+    }
+
+    /**
+     * Pull the order data from the event
+     *
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        try {
+            $order = $observer->getEvent()->getOrder();
+            /* @var MagentoOrder $order */
+            $this->_queue->scheduleJob(OrderHandler::class, 'handle', [$order->getStoreId(), $order->getId()]);
+            $this->_logger->debug("Scheduled sync job for product ID: {$order->getId()}, Store ID: {$order->getStoreId()}");
+        } catch (\Exception $e) {
+            $this->_logger->debug($e->getMessage());
+        }
+    }
+
+
+}
