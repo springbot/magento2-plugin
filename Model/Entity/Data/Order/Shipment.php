ff7c745824fe05aed4ffd71da302f3b9661d1f75
Evan Jacobs <ejacobs@springbot.com>
2016-09-22 14:18:09 -0400

    Incremented version for update script

---
+++ Model/Entity/Data/Order/Shipment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Springbot\Main\Model\Entity\Data\Order;
+
+use Springbot\Main\Api\Entity\Data\Order\ShipmentInterface;
+
+/**
+ * Class Shipment
+ * @package Springbot\Main\Model\Entity\Data\Order
+ */
+class Shipment implements ShipmentInterface
+{
+
+
+}
