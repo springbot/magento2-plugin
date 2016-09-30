1205019b0fc5358e3d4df4bcf58c1d4a112bcac0
Evan Jacobs <ejacobs@springbot.com>
2016-09-30 09:21:40 -0400

    Modifications to Springbot category model

--- Model/Api/Entity/Data/Category.php
+++ Model/Api/Entity/Data/Category.php
@@ -9,46 +9,162 @@ use Springbot\Main\Api\Entity\Data\CategoryInterface;
  * Class Category
  * @package Springbot\Main\Model\Api\Entity\Data
  */
-class Category extends MagentoCategory implements CategoryInterface
+class Category implements CategoryInterface
 {
+
+    public $categoryId;
+    public $level;
+    public $path;
+    public $createdAt;
+    public $updatedAt;
+    public $categoryAttributes = [];
+
+    public $name;
+    public $urlPath;
+    public $description;
+
+    /**
+     * Category constructor.
+     * @param $categoryId
+     * @param $level
+     * @param $path
+     * @param $createdAt
+     * @param $updatedAt
+     */
+    public function setValues($categoryId, $level, $path, $createdAt, $updatedAt)
+    {
+        $this->categoryId = $categoryId;
+        $this->level = $level;
+        $this->path = $path;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+        $this->loadAttributes();
+    }
+
+    /**
+     * @return mixed
+     */
     public function getCategoryId()
     {
-        return parent::getEntityId();
+        return $this->categoryId;
     }
 
-    public function getPath()
-    {
-        return parent::getPath();
-    }
-
+    /**
+     * @return mixed
+     */
     public function getLevel()
     {
-        return parent::getLevel();
+        return $this->level;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
     }
 
     public function getName()
     {
-        return parent::getName();
-    }
-
-    public function getUrl()
-    {
-        return parent::getUrl();
+        return $this->name;
     }
 
     public function getUrlPath()
     {
-        return parent::getUrlKey();
+        return $this->urlPath;
+    }
+
+    public function getUrl()
+    {
+        // TODO: Implement getUrl() method.
     }
 
     public function getIsActive()
     {
-        return parent::getIsActive();
+        // TODO: Implement getIsActive() method.
     }
 
     public function getDescription()
     {
-        return parent::getDescription();
+        return $this->description;
+    }
+
+    private function loadAttributes()
+    {
+        $conn = $this->connectionResource->getConnection();
+        $query = $conn->query("SELECT 
+              ea.attribute_code,
+              cpet.value as `text`,
+              cped.value as `datetime`,
+              cpedec.value as `decimal`,
+              cpei.value as `int`,
+              cpev.value as `varchar`
+            FROM {$conn->getTableName('eav_attribute')} ea
+            LEFT JOIN {$conn->getTableName('catalog_category_entity_text')} cpet
+                ON (ea.attribute_id = cpet.attribute_id)
+            LEFT JOIN {$conn->getTableName('catalog_category_entity_datetime')} cped
+                ON (ea.attribute_id = cped.attribute_id)
+            LEFT JOIN {$conn->getTableName('catalog_category_entity_decimal')} cpedec
+                ON (ea.attribute_id = cpedec.attribute_id)
+            LEFT JOIN {$conn->getTableName('catalog_category_entity_int')} cpei
+                ON (ea.attribute_id = cpei.attribute_id)
+            LEFT JOIN {$conn->getTableName('catalog_category_entity_varchar')} cpev
+                ON (ea.attribute_id = cpev.attribute_id)
+            WHERE cpet.entity_id = :entity_id 
+             OR cped.entity_id = :entity_id
+             OR cpedec.entity_id = :entity_id
+             OR cpei.entity_id = :entity_id 
+             OR cpev.entity_id = :entity_id
+        ", ['entity_id' => $this->categoryId]);
+
+        foreach($query->fetchAll() as $attributeRow) {
+            $value = null;
+            if (isset($attributeRow['text'])) {
+                $value = $attributeRow['text'];
+            }
+            else if (isset($attributeRow['datetime'])) {
+                $value = $attributeRow['datetime'];
+            }
+            else if (isset($attributeRow['decimal'])) {
+                $value = $attributeRow['decimal'];
+            }
+            else if (isset($attributeRow['int'])) {
+                $value = $attributeRow['int'];
+            }
+            else if (isset($attributeRow['varchar'])) {
+                $value = $attributeRow['varchar'];
+            }
+
+            switch($attributeRow['attribute_code']) {
+                case 'name':
+                    $this->name = $value;
+                    break;
+                case 'url_path':
+                    $this->urlPath = $value;
+                    break;
+                default:
+                    if ($value !== null) {
+
+                    }
+            }
+        }
     }
 
 
