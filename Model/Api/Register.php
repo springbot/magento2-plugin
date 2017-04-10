eb0ef4bcb6080cd0b1135593e523c8708837e1e2
Evan Jacobs <ejacobs@springbot.com>
2017-04-10 17:59:48 -0400

    Added register model factory to store register endpoint

--- Model/Api/Register.php
+++ Model/Api/Register.php
@@ -11,11 +11,23 @@ use Springbot\Main\Api\RegisterInterface;
  */
 class Register implements RegisterInterface
 {
+    private $registerFactory;
+
+    /**
+     * @param RegisterFactory $registerFactory
+     */
+    public function __construct(RegisterFactory $registerFactory)
+    {
+        $this->registerFactory = $registerFactory;
+    }
 
     public function registerStores()
     {
+        $registerModel = $this->registerFactory->create();
+
+        // TODO: Register model needs to split out so authenticate and register are two separate methods
+
         return $this;
     }
-
 
 }
