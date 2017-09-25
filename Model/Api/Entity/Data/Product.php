2916cbd3f65b28188ae1e4f15d3ca59ac9099e90
dkirby@springbot.com <dkirby@springbot.com>
2017-09-25 13:32:38 -0400

    Fixes resourceConnection issue

--- Model/Api/Entity/Data/Product.php
+++ Model/Api/Entity/Data/Product.php
@@ -319,7 +319,7 @@ class Product implements ProductInterface
     public function getParentSkus()
     {
         $idColumnName = $this->getIdColumnName();
-        $resource =  $this->resourceConnection;
+        $resource =  $this->connectionResource;
         $conn = $resource->getConnection();
         $query = $conn->query("SELECT cpe.sku FROM {$resource->getTableName('catalog_product_relation')} cper
             LEFT JOIN {$resource->getTableName('catalog_product_entity')} cpe
@@ -336,7 +336,7 @@ class Product implements ProductInterface
     private function loadAttributes()
     {
         $idColumnName = $this->getIdColumnName();
-        $resource =  $this->resourceConnection;
+        $resource =  $this->connectionResource;
         $conn = $resource->getConnection();
         $query = $conn->query("
             SELECT ea.attribute_code AS `code`, eav.value  AS 'value'
@@ -354,7 +354,7 @@ class Product implements ProductInterface
             SELECT ea.attribute_code AS `code`, eav.value AS 'value'
             FROM {$resource->getTableName('catalog_product_entity')} cpe
               LEFT JOIN {$resource->getTableName('catalog_product_entity_int')} eav ON (cpe.{$idColumnName} = eav.{$idColumnName})
-              LEFT JOIN {$resource->getTableName('eav_attribute')} ea ON (eav.attribute_id = ea.attribute_id)
+              LEFT JOIN {$resource->getTableName('eav_attresourceConnectionribute')} ea ON (eav.attribute_id = ea.attribute_id)
             WHERE (cpe.{$idColumnName} = :{$idColumnName})
             UNION
             SELECT ea.attribute_code AS `code`, eav.value AS 'value'
@@ -423,7 +423,7 @@ class Product implements ProductInterface
     private function loadCategories()
     {
         $idColumnName = $this->getIdColumnName();
-        $resource =  $this->resourceConnection;
+        $resource =  $this->connectionResource;
         $conn = $resource->getConnection();
         $query = $conn->query("SELECT * FROM {$resource->getTableName('catalog_category_product')}  ccp
           LEFT JOIN catalog_category_entity cce ON (ccp.category_id = cce.{$idColumnName})
@@ -453,4 +453,4 @@ class Product implements ProductInterface
     }
 
 
-} 
+}
