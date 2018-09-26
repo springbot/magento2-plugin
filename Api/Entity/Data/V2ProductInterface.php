71788dfd465c780599d449d05ab8f1e299cbebd6
Joey Kimsey <JoeyK4816@users.noreply.github.com>
2018-09-26 14:58:58 -0400

    fix conflicts (#39)
    * fix conflicts
    
    Wrong files upload last time.
    
    * Update to resolve Swagger error
    
    To resolve reported issue #35

--- Api/Entity/Data/V2ProductInterface.php
+++ Api/Entity/Data/V2ProductInterface.php
@@ -2,155 +2,117 @@
 namespace Springbot\Main\Api\Entity\Data;
 
 /**
- * Interface ProductInterfaceV2
+ * Interface V2ProductInterface
  * @package Springbot\Main\Api
  */
-interface ProductInterfaceV2
+interface V2ProductInterface
 {
-    /**
-     * @param $storeId
-     * @param $productId
-     * @param $sku
-     * @param $type
-     * @param $createdAt
-     * @param $updatedAt
-     * @param $customAttributeSetId
-     * @return void
-     */
-    public function setValues($storeId, $productId, $sku, $type, $createdAt, $updatedAt, $customAttributeSetId);
-
     /**
      * @return int
      */
     public function getProductId();
-
     /**
      * @return string
      */
     public function getName();
-
     /**
      * @return string
      */
     public function getUrlKey();
-
     /**
      * @return string
      */
     public function getSku();
-
     /**
      * @return string
      */
     public function getType();
-
     /**
      * @return string
      */
     public function getStatus();
-
     /**
      * @return string
      */
     public function getVisibility();
-
     /**
      * @return string
      */
     public function getMsrp();
-
     /**
      * @return string
      */
     public function getPrice();
-
     /**
      * @return string
      */
     public function getCost();
-
     /**
      * @return string
      */
     public function getWeight();
-
     /**
      * @return string
      */
     public function getCreatedAt();
-
     /**
      * @return string
      */
     public function getUpdatedAt();
-
     /**
      * @return string
      */
     public function getDescription();
-
     /**
      * @return string
      */
     public function getShortDescription();
-
     /**
      * @return array
      */
     public function getCategoryIds();
-
     /**
      * @return array
      */
     public function getRootCategoryIds();
-
     /**
      * @return array
      */
     public function getAllCategoryIds();
-
     /**
      * @return string
      */
     public function getSpecialPrice();
-
     /**
      * @return string
      */
     public function getDefaultUrl();
-
     /**
      * @return string
      */
     public function getUrlInStore();
-
     /**
      * @return string
      */
     public function getUrlIdPath();
-
     /**
      * @return string
      */
     public function getImageUrl();
-
     /**
      * @return string
      */
     public function getImageLabel();
-
     /**
      * @return array
      */
     public function getParentSkus();
-
     /**
      * @return array
      */
     public function getCustomAttributeSetId();
-
     /**
-     * @return \Springbot\Main\Api\Entity\Data\Product\ProductAttributeInterfaceV2[]
+     * @return \Springbot\Main\Api\Entity\Data\Product\V2ProductAttributeInterface[]
      */
     public function getProductAttributes();
 }
--- Model/Api/Entity/ProductRepositoryV2.php
+++ Model/Api/Entity/ProductRepositoryV2.php
@@ -4,7 +4,7 @@ namespace Springbot\Main\Model\Api\Entity;
 
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Model\AbstractModel;
-use Springbot\Main\Api\Entity\ProductRepositoryInterfaceV2;
+use Springbot\Main\Api\Entity\V2ProductRepositoryInterface;
 use Magento\Framework\App\Request\Http as HttpRequest;
 use Magento\Framework\App\ResourceConnection;
 use Springbot\Main\Model\Api\Entity\Data\ProductFactory;
@@ -14,7 +14,7 @@ use Magento\Store\Model\StoreManagerInterface;
  * Class ProductRepository
  * @package Springbot\Main\Model\Api\Entity
  */
-class ProductRepositoryV2 extends AbstractRepository implements ProductRepositoryInterfaceV2
+class V2ProductRepository extends AbstractRepository implements V2ProductRepositoryInterface
 {
     /* @var ProductFactory $productFactory */
     protected $productFactory;
@@ -55,37 +55,19 @@ class ProductRepositoryV2 extends AbstractRepository implements ProductRepositor
         $this->filterResults($select);
         $ret = [];
         foreach ($conn->fetchAll($select) as $row) {
-            $ret[] = $this->createProduct($storeId, $row);
+            $ret[] = $this->getFromId($storeId, $row['entity_id']);
         }
         return $ret;
     }
 
     public function getFromId($storeId, $productId)
     {
-        $resource =  $this->resourceConnection;
-        $conn = $resource->getConnection();
-        $select = $conn->select()
-            ->from([$resource->getTableName('catalog_product_entity')])
-            ->where('entity_id = ?', $productId);
-
-        foreach ($conn->fetchAll($select) as $row) {
-            return $this->createProduct($storeId, $row);
-        }
-        return null;
+        return $this->getSpringbotModel()->load($productId);
     }
 
-    private function createProduct($storeId, $row)
+    public function getSpringbotModel()
     {
-        $product = $this->productFactory->create();
-        $product->setValues(
-            $storeId,
-            $row['entity_id'],
-            $row['sku'],
-            $row['type_id'],
-            $row['created_at'],
-            $row['updated_at'],
-            $row['attribute_set_id']
-        );
-        return $product;
+        $om = ObjectManager::getInstance();
+        return $om->create('Springbot\Main\Model\Api\Entity\Data\V2Product');
     }
 }
