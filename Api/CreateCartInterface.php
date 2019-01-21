89b785de1eb0d22c02baf7e47214ad70ff6d8ee2
Joey Kimsey <joeyk4816@gmail.com>
2019-01-21 16:57:46 -0500

    Add required files for new endpoint

---
+++ Api/CreateCartInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Springbot\Main\Api;
+
+/**
+ * Interface CreateCartInterface
+ * @package Springbot\Main\Api
+ */
+interface CreateCartInterface
+{
+
+    /**
+     * @return \Springbot\Main\Api\CreateCartInterface
+     * @throws \Exception
+     */
+    public function getCart(
+    );
+}
