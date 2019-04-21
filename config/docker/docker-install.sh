0628d73b9d426909a79a31d76d3dde3ece3ac0be
Evan Jacobs <evan.jacobs2@gmail.com>
2019-04-21 00:18:09 -0400

    More docker automation

--- config/docker/docker-install.sh
+++ config/docker/docker-install.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
+# this script is for testing and development purposes only
+
+# wait for mysql server to become available
+while ! mysqladmin ping -h"mysql" --silent; do
+    sleep 1
+done
+
+# create the database
+mysql -h mysql -u root -e "CREATE DATABASE magento2;"
+mysql -h mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'springbot'@'%'; FLUSH PRIVILEGES;"
+
+# install magento and sample data
+install-magento
+install-sampledata
+
+# enable our plugin and clean the cache
 su www-data -s /bin/bash -c 'php bin/magento module:enable Springbot_Queue'
 su www-data -s /bin/bash -c 'php bin/magento module:enable Springbot_Main'
 su www-data -s /bin/bash -c 'php bin/magento setup:upgrade'
 su www-data -s /bin/bash -c 'php bin/magento cache:clean'
+
+# Override the default plugin settings to point to our docker hosts
+mysql -h mysql -uspringbot -ppassword magento2 -e "INSERT INTO core_config_data (config_id, scope, scope_id, path, value) VALUES (NULL , 'default', '0', 'springbot/configuration/assets_domain', '$ASSETS_DOMAIN');"
+mysql -h mysql -uspringbot -ppassword magento2 -e "INSERT INTO core_config_data (config_id, scope, scope_id, path, value) VALUES (NULL , 'default', '0', 'springbot/configuration/app_url', '$APP_URL');"
+mysql -h mysql -uspringbot -ppassword magento2 -e "INSERT INTO core_config_data (config_id, scope, scope_id, path, value) VALUES (NULL , 'default', '0', 'springbot/configuration/api_url', '$ETL_URL');"
---
+++ config/docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# this script is for testing and development purposes only
+
+if [ ! -f "/var/installed" ]; then
+
+    # the install script needs the webserver to be running, so run it in the background
+    /springbot/docker-install.sh &
+
+    touch /var/installed
+
+fi
+
+# run the webserver
+/sbin/my_init
