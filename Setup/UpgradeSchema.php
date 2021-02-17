796c4b22e723e7401c57fb5f7e4f7d6a4d3ad391
Josh Lucio <jlucio@springbot.com>
2021-02-17 15:09:42 -0500

    Correct spacing

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -15,13 +15,13 @@ class UpgradeSchema implements UpgradeSchemaInterface
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-		
-		/**
-		 *  Drop old tables that are no longer used SB3-243
-		 */
+
+        /**
+        *  Drop old tables that are no longer used SB3-243
+        */
         $connection = $setup->getConnection();
-		$connection->dropTable($connection->getTableName('springbot_trackable'));
-		$connection->dropTable($connection->getTableName('springbot_marketplaces_remote_order'));
+        $connection->dropTable($connection->getTableName('springbot_trackable'));
+        $connection->dropTable($connection->getTableName('springbot_marketplaces_remote_order'));
 
         $setup->endSetup();
     }
